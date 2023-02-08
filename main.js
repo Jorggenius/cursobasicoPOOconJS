3815ec9d4a702e27cec5122f8762cf7d3de1993a
jorge luis <martinezmonajorgeluis@gmail.com>
2023-02-08 18:07:21 -0500

    este es el segundo commit de la clase # 7 (ventajas de la programacion orientada a objetos) en esta segunda parte de la clase tve que desarrollar una clase y tres instancias

--- main.js
+++ main.js
@@ -1,67 +1,68 @@
-const juan1 = {
-    name: "JuanDC",
-    username: "juandc",
-    points: 100,
-    socialMedia: {
-        twiter: "fjuandc",
-        instagram: "fjuandc",
-        facebook: undefined,
-    },
-    approvedCourses: [
-        "Curso definitivo de html y css",
-        "Curso Practico de html y css",
-    ],
-    learningPaths: [
-    {
-        name: "Escuela de desarrollo web",
-        courses: [
-            "Curso definitivo de html y css",
-            "Curso Practico de html y css",
-            "Curso de responsive desing",
-            "Curso de Sistemas de diseño",
-        ],
-    },
-    {
-        name: "Escuela de videojuegos",
-        courses: [
-            "Curso de introduccion a la produccion de VGs",
-            "Curso de unreal engine",
-            "Curso de Unioty 3D",
-        ],
-    },
-    ],
+class learningPhats{
+    constructor({
+        approvedCourses = []
+    }){
+        this.approvedCourses = [] = approvedCourses;
+    }
 }
 
-const miguelito1 =  {
-    name: "Miguelito",
-    username: "miguelitofeliz",
-    points: 1000,
-    socialMedia: {
-        twiter: "miguelitofeliz",
-        instagram: "miguelito_feliz",
-        facebook: undefined,
-    },
+const escuelaWeb = new learningPhats({
     approvedCourses: [
-        "Curso DataBusiness",
-        "Curso DataViz",
-    ],
-    learningPaths: [
-    {
-        name: "Escuela de desarrollo web",
-        courses: [
-            "Curso definitivo de html y css",
+        "Curso definitivo de html y css",
             "Curso Practico de html y css",
             "Curso de responsive desing",
             "Curso de Sistemas de diseño",
-        ],
-    },
-    {
-        name: "Escuela de Data science",
-        courses: [
-            "Curso DataBusiness",
-            "Curso DataViz",
-            "Curso Tableau",
-        ],
-    },
-    ],
+    ]
+})
+const escuelaData = new learningPhats({
+    approvedCourses:[
+        "Curso DataBusiness",
+        "Curso DataViz",
+        "Curso Tableau",
+    ]
+})
+const escuelaVgs = new learningPhats({
+    approvedCourses:[
+        "Curso de introduccion a la produccion de VGs",
+        "Curso de unreal engine",
+        "Curso de Unioty 3D", 
+    ]
+})
+
+class Student{
+    constructor({
+        name,
+        email,
+        username,
+        twiter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+        learningPhats = [],
+    }){
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.socialMedia = {
+            twiter,
+            instagram,
+            facebook,
+        };
+        this.approvedCourses = [] = approvedCourses;
+        this.learningPhats = [] = learningPhats;
+    }
 }
+
+const juan2 = new Student({
+    name: "JuanDc",
+    username: "juandc",
+    email: "juanito@platzi.com",
+    twiter: "fjuandc",
+    
+})
+const miguelieto22 = new Student({
+    name: "Miguelito",
+    username: "miguelitofiliz",
+    email: "miguelito@platzi.com",
+    instagram: "miguelito_feliz",
+})
